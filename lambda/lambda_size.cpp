27ce7d90053587e895144c7ebc89ea032b484979
GYMK <gymk.tech@gmail.com>
2018-07-23 16:03:21 +0900

    lambda [=] study, std::thread class member invocation study

--- lambda/lambda_size.cpp
+++ lambda/lambda_size.cpp
@@ -13,17 +13,21 @@ int main()
     auto f3 = [ar](){}; // lambda having pass by value
     std::cout << "Size of lambda [ar](){} : " << sizeof(f3) << std::endl;
 
-    auto f4 = [=](){}; // all by value
-    std::cout << "Size of lambda [=](){} : " << sizeof(f4) << std::endl;
+    auto f4 = [=](){}; // all by value, std::array is not used, so ar is not passed to lamba
+    std::cout << "Size of lambda [=](){} ar not used : " << sizeof(f4) << std::endl;
 
-    auto f5 = [&](){}; // lambda having pass by value
-    std::cout << "Size of lambda [&](){} : " << sizeof(f5) << std::endl;
+    auto f5 = [=](){ std::cout << ar.empty() << std::endl; }; // all by value - std::array is used, ar getting passed to lambda
+    std::cout << "Size of lambda [=](){} ar used : " << sizeof(f5) << std::endl;
+
+    auto f6 = [&](){}; // lambda having pass by value
+    std::cout << "Size of lambda [&](){} : " << sizeof(f6) << std::endl;
 }
 
 /*
 Size of lambda [](){} : 1
 Size of lambda [&ar](){} : 8
 Size of lambda [ar](){} : 100
-Size of lambda [=](){} : 1
+Size of lambda [=](){} ar not used : 1
+Size of lambda [=](){} ar used : 100
 Size of lambda [&](){} : 1
 */
--- thread/thread3.cpp
+++ thread/thread3.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <thread>
 
-int main()
+// Running a lambda function as a std::thread
+void lambda_as_thread(void)
 {
 	std::thread th([]{
 			for(int i = 0; i < 1000; i++)
@@ -12,4 +13,28 @@ int main()
 		std::cout << std::this_thread::get_id() << " Main funciton executing\n";
 
 	th.join();
+}
+
+// Running a class member function which is non-static as std::thread
+void class_member_as_thread(void)
+{
+	class CTest
+	{
+	public:
+		void PrintTest(void)
+		{
+			std::cout << "Member function as thread\n";
+		}
+	};
+
+	CTest test;
+
+	std::thread th(&CTest::PrintTest, &test);	// without & to function, it is not compiling
+	th.join();
+}
+
+int main()
+{
+	lambda_as_thread();
+	class_member_as_thread();
 }
