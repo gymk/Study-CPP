3f6de56f2d953ff669524ed2d55b6508d2420521
GYMK <gymk.tech@gmail.com>
2018-08-02 19:57:01 +0900

    stl sorting

---
+++ stl/sorting/stable_sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<std::string> sl { "effect", "effective", "effectiveness", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+    std::stable_sort(
+        sl.begin(),
+        sl.end(),
+        [](const std::string & l, const std::string & r){ return l.size() < r.size(); }
+        );
+
+    for(auto & m : sl)
+        std::cout << m << std::endl;
+}
+
+/*
+Output:
+
+one
+two
+six
+ten
+four
+five
+nine
+three
+seven
+eight
+effect
+effective
+effectiveness
+
+*/
+
+/*
+Links:
+    *) TO DO https://www.youtube.com/watch?v=zt7ThwVfap0
+*/
